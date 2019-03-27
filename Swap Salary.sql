635e03b4f3c9eb992e991baee487c9ff48ea95fb
moulya-somasundara <moulya.sundara@heavywater.solutions>
2019-03-26 21:58:49 -0400

    first question

--- Swap Salary.sql
+++ Swap Salary.sql
@@ -1,4 +1,5 @@
-Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values (i.e., change all f values to m and vice versa) with a single update statement and no intermediate temp table.
+Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values 
+(i.e., change all f values to m and vice versa) with a single update statement and no intermediate temp table.
 
 Note that you must write a single update statement, DO NOT write any select statement for this problem.
 
